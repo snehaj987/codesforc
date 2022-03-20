44922a29d1309bec2a230b761a951395afaed5fa
snehaj987 <101989629+snehaj987@users.noreply.github.com>
2022-03-20 23:36:12 +0530

    Update Variables.c

--- Variables.c
+++ Variables.c
@@ -6,7 +6,7 @@ int main()
     char s = 'c';
     char *str = "line";
     float f = 23.4;
-    printf("siddhant\n");
+    printf("sneha\n");
     printf("%d\n",a);
     printf("%f\n",f);
     printf("%c\n",s);
