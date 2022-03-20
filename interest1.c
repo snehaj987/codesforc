c04572381e9cc42c68100bce45182863bd1212b4
snehaj987 <101989629+snehaj987@users.noreply.github.com>
2022-03-21 00:08:45 +0530

    Update interest1.c

--- interest1.c
+++ interest1.c
@@ -5,7 +5,7 @@
 int main()
 { 
     float p, t, r, si, ci;
-	printf("siddhant\n");
+	printf("sneha\n");
 	printf("Enter principal amount (p): ");
 	scanf("%f", &p);
 	printf("Enter time in year (t): ");
